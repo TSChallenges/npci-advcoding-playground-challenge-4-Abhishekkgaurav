76867b21940e756d660776882600f898cdab90a9
Abhishek Kumar <60622439+Abhishekkgaurav@users.noreply.github.com>
2025-02-22 11:06:06 +0000

    Playground Challenge Completed, Group 2

--- src/main/java/com/bankmgmt/app/rest/BankController.java
+++ src/main/java/com/bankmgmt/app/rest/BankController.java
@@ -7,33 +7,77 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import com.bankmgmt.app.entity.Account;
+import com.bankmgmt.app.service.BankService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 // TODO: Make this class a Rest Controller
+@RestController
+@RequestMapping("/accounts")
 public class BankController {
 
     // TODO Autowired the BankService class
+    @Autowired
     private BankService bankService;
 
     // TODO: API to Create a new account
-    
+    @PostMapping
+    public ResponseEntity<Account> createAccount(@RequestBody Account account) {
+        Account newAccount = bankService.createAccount(account.getAccountHolderName(), account.getAccountType(), account.getEmail());
+        return new ResponseEntity<>(newAccount, HttpStatus.CREATED);
+    }
+
 
     // TODO: API to Get all accounts
-    
+    @GetMapping
+    public List<Account> getAllAccounts() {
+        return bankService.getAllAccounts();
+    }
 
     // TODO: API to Get an account by ID
-    
+    @GetMapping("/{id}")
+    public ResponseEntity<Account> getAccountById(@PathVariable int id) {
+        return bankService.getAccountById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
 
     // TODO: API to Deposit money
-    
+    @PostMapping("/{id}/deposit")
+    public ResponseEntity<String> deposit(@PathVariable int id, @RequestParam double amount) {
+        return bankService.deposit(id, amount) ?
+                ResponseEntity.ok("Deposit successful") :
+                ResponseEntity.badRequest().body("Invalid deposit amount");
+    }
+
 
     // TODO: API to Withdraw money
-    
+    @PostMapping("/{id}/withdraw")
+    public ResponseEntity<String> withdraw(@PathVariable int id, @RequestParam double amount) {
+        return bankService.withdraw(id, amount) ?
+                ResponseEntity.ok("Withdrawal successful") :
+                ResponseEntity.badRequest().body("Insufficient funds or invalid amount");
+    }
 
     // TODO: API to Transfer money between accounts
-    
+    @PostMapping("/transfer")
+    public ResponseEntity<String> transfer(@RequestParam int fromId, @RequestParam int toId, @RequestParam double amount) {
+        return bankService.transfer(fromId, toId, amount) ?
+                ResponseEntity.ok("Transfer successful") :
+                ResponseEntity.badRequest().body("Transfer failed: check balance and account details");
+    }
 
     // TODO: API to Delete an account
-    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAccount(@PathVariable int id) {
+        return bankService.deleteAccount(id) ?
+                ResponseEntity.noContent().build() :
+                ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
     
 }
--- src/main/java/com/bankmgmt/app/service/BankService.java
+++ src/main/java/com/bankmgmt/app/service/BankService.java
@@ -3,34 +3,111 @@ package com.bankmgmt.app.service;
 import com.bankmgmt.app.entity.Account;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import com.bankmgmt.app.entity.Account;
+import org.springframework.stereotype.Service;
+
 import java.sql.SQLOutput;
 import java.util.*;
 
-
+@Service
 public class BankService {
 
     private List<Account> accounts = new ArrayList<>();
     private Integer currentId = 1;
 
     // TODO: Method to Create a new Account
-    
+
+    public Account createAccount(String accountHolderName, String accountType, String email) {
+
+        Account account = new Account(currentId++, accountHolderName, accountType, 0.0, email);
+        for (Account existingAccount : accounts) {
+            if (existingAccount.getEmail().equalsIgnoreCase(account.getEmail())) {
+                currentId--;
+                throw new IllegalArgumentException("Email already in use.");
+            }
+        }
+        if (!account.getAccountType().equalsIgnoreCase("SAVINGS") &&
+                !account.getAccountType().equalsIgnoreCase("CURRENT")) {
+            currentId--;
+            throw new IllegalArgumentException("Invalid account type. Must be SAVINGS or CURRENT.");
+        }
+        accounts.add(account);
+        return account;
+    }
+
 
     // TODO: Method to Get All Accounts
-    
+    public List<Account> getAllAccounts() {
+        return accounts;
+    }
+
+
 
     // TODO: Method to Get Account by ID
-    
+
+    public Optional<Account> getAccountById(int id) {
+        return accounts.stream().filter(a -> a.getId() == id).findFirst();
+    }
+
+
 
     // TODO: Method to Deposit Money to the specified account id
-    
+
+    public boolean deposit(int id, double amount) {
+        if (amount <= 0) return false;
+        Optional<Account> accountOpt = getAccountById(id);
+        if (accountOpt.isPresent()) {
+            Account account = accountOpt.get();
+            account.setBalance(account.getBalance() + amount);
+            return true;
+        }
+        return false;
+    }
+
 
     // TODO: Method to Withdraw Money from the specified account id
-    
+    public boolean withdraw(int id, double amount) {
+        if (amount <= 0) return false;
+        Optional<Account> accountOpt = getAccountById(id);
+        if (accountOpt.isPresent()) {
+            Account account = accountOpt.get();
+            if (account.getBalance() >= amount) {
+                account.setBalance(account.getBalance() - amount);
+                return true;
+            }
+        }
+        return false;
+    }
+
 
     // TODO: Method to Transfer Money from one account to another
-    
-    
-    // TODO: Method to Delete Account given a account id
 
+    public boolean transfer(int fromId, int toId, double amount) {
+        if (amount <= 0) return false;
+        Optional<Account> fromAccountOpt = getAccountById(fromId);
+        Optional<Account> toAccountOpt = getAccountById(toId);
+
+        if (fromAccountOpt.isPresent() && toAccountOpt.isPresent()) {
+            Account fromAccount = fromAccountOpt.get();
+            Account toAccount = toAccountOpt.get();
+            if (fromAccount.getBalance() >= amount) {
+                fromAccount.setBalance(fromAccount.getBalance() - amount);
+                toAccount.setBalance(toAccount.getBalance() + amount);
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
+    // TODO: Method to Delete Account given a account id
+    public boolean deleteAccount(int id) {
+        return accounts.removeIf(account -> account.getId() == id);
+    }
 
 }
